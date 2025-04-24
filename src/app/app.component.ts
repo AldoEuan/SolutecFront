d80ce3bcd33fdb51e6fe4b48c71b56710d57fbbc
Aldo Euan <80072409+AldoEuan@users.noreply.github.com>
2025-04-24 16:00:15 -0600

    Creando pantalla Inicio
    se creo  la vista de inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
